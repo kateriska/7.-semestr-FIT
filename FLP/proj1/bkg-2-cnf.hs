f281ac08ad6d753f7454d705a319e2dd171998ce
Kateřina Fořtová <43783793+kateriska@users.noreply.github.com>
2021-02-17 10:21:08 +0100

    show correctly rules from inner representation

--- FLP/proj1/bkg-2-cnf.hs
+++ FLP/proj1/bkg-2-cnf.hs
@@ -35,6 +35,9 @@ checkSyntaxNonterminalsOverall xs
 listToTuple :: [a] -> (a,a)
 listToTuple [x,y] = (x,y)
 
+showRule :: ([Char], [Char]) -> [Char]
+showRule (x, y) = (x ++ "->" ++ y)
+
 
 
 
@@ -46,17 +49,17 @@ data CFG_t = CFG_t
     terminal_symbols :: [String],
     starting_symbol :: String,
     grammar_rules :: [([Char], [Char])]
-  } deriving (Eq, Read, Show)
+  } deriving (Eq, Read)
 
-{-
+--{-
 instance Show CFG_t where
   show (CFG_t nonterminal_symbols terminal_symbols starting_symbol grammar_rules) =
     (intercalate "," nonterminal_symbols) ++ "\n" ++
     (intercalate "," terminal_symbols) ++ "\n" ++
-    starting_symbol ++
-    grammar_rules
+    starting_symbol ++ "\n" ++
+    (unlines (map showRule grammar_rules))
 
--}
+---}
 parseCFG :: String -> CFG_t
 parseCFG grammar_input = CFG_t {
 nonterminal_symbols = (splitOn "," (lines (grammar_input) !! 0)),
