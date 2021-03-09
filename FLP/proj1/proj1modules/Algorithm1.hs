9ddc9d3e0e7734c31bbaca3a5df1234238be82c8
Kateřina Fořtová <43783793+kateriska@users.noreply.github.com>
2021-03-09 17:15:16 +0100

    comments added

--- FLP/proj1/proj1modules/Algorithm1.hs
+++ FLP/proj1/proj1modules/Algorithm1.hs
@@ -1,3 +1,10 @@
+-- Project name: bkg-2-cnf
+-- Author: Katerina Fortova
+-- Login: xforto00
+-- Year: 2020 / 2021
+
+-- Module description: Implementation of Algorithm 4.5 - Remove of simple rules
+
 module Algorithm1
   (recursionNA, createNA, checkSimpleRule, isNontermForNA,
   changeLeftSideRule, filterRules, removeSimpleRules, iterateList, processAlgorithm1)
@@ -12,72 +19,46 @@ import Data.Tuple
 import Types
 import Parser
 
+-- Recursively add new nonterms to NA set
 recursionNA :: [([Char], [Char])] -> Int -> [[Char]] -> [[Char]] -> [[Char]]
---recursionNA rules input_set nonterminals = if
---recursionNA rules input_set nonterminals = do let value1 = recursionNA (rules) (concat (map (createNA rules input_set) nonterminals)) (concat (map (createNA rules input_set) nonterminals))
---                                              let value2 = recursionNA (rules) (value1) (value2)
-
---                                              if value1 == value2
---                                                then value2
--- init counter je pocet vsech pravidel, snizovani az na nulu a pak konec
+-- init counter is count of all rules, decrement to zero and then end
 recursionNA rules 0 input_set nonterminals = input_set
 recursionNA rules counter input_set nonterminals = recursionNA (rules) (counter - 1) (nub(concat (map (createNA rules input_set) nonterminals))) (nub(concat (map (createNA rules input_set) nonterminals)))
---recursionNA rules input_set nonterminals = nub (concat (map (createNA rules input_set) nonterminals))
---recursionNA rules input_set nonterminals = concat (map (createNA rules []) nonterminals)
 
-
+-- Create NA set
 createNA :: [([Char], [Char])] -> [[Char]] -> [Char] -> [[Char]]
 --createNA rules [] nonterminal  = (concat (filter (/= []) (map (isNontermForNA []) (filter ((==nonterminal).fst) rules ))))
 createNA rules input_set nonterminal  = (concat (filter (/= []) (map (isNontermForNA input_set) (filter ((==nonterminal).fst) rules ))))
 
-
-
---createNA :: Char -> [([Char], [Char])] -> [[Char]] -> [[Char]]
---createNA nonterminal rules input_set = map checkSimpleRule(filter ((==nonterminal).fst) rules)
-
+-- Check whether rule is simple (A -> B)
 checkSimpleRule :: ([Char], [Char]) -> Bool
 checkSimpleRule rule
  | isValidStartingSymbol (snd rule) == True = True
  | otherwise = False
 
---checkSimpleRules :: [([Char], [Char])] -> [Bool]
---checkSimpleRules rules = map checkSimpleRule rules
-
---addToNA :: [([Char], [Char])] -> [Bool] -> [[Char]] -> [[Char]]
---addToNA rules simple_rules_bool input_set = map addNonterminalToNA
-
-
---addNonterminalToNA :: ([Char], [Char]) -> Bool -> [[Char]] -> [[Char]]
---addToNA rule is_simple_rule input_set
---  | is_simple_rule == True = input_set ++ [(snd rule)]
---  | otherwise = input_set
-
+-- Add nonterm to NA if the rule is simple
 isNontermForNA :: [[Char]] -> ([Char], [Char]) -> [[Char]]
---isNontermForNA [] rule
---  | checkSimpleRule rule == True = [(snd rule)]
---  | otherwise = []
 isNontermForNA input_set rule
  | checkSimpleRule rule == True = input_set ++ [(snd rule)]
  | otherwise = input_set
 
---removeSimpleRules(CFG_t nonterminal_symbols terminal_symbols starting_symbol grammar_rules) = map (recursionNA (rules) (length grammar_rules) ) nonterminal_symbols nonterminal_symbols
---filterRules x = filter ((==x).fst)  rules
+-- Change left side of rule
 changeLeftSideRule :: [Char] -> ([Char], [Char]) -> ([Char], [Char])
 changeLeftSideRule nonterminal (x,y) = (nonterminal, y)
---concat (map filterRules ["E","F"])
 
+-- Filter rules with chosen nonterminal in the left side
 filterRules :: [([Char], [Char])] -> [Char] -> [([Char], [Char])]
 filterRules rules nonterminal = filter ((==nonterminal).fst)  rules
 
+-- Remove all simple rules from rule set
 removeSimpleRules :: [Char] -> [([Char], [Char])] -> [[Char]] -> [([Char], [Char])]
 removeSimpleRules nonterminal rules na_set = map (changeLeftSideRule nonterminal) (filter (\n ->  checkSimpleRule n == False) (concat (map (filterRules rules) na_set)) )
 
---accumulateTransformedRules new_rules [] = new_rules
---accumulateTransformedRules new_rules final_set = new_rules ++ final_set
+-- Loop through list of all nonterms and their rules - compute NA sets for all nonterms
 iterateList :: [String] -> [([Char], [Char])] -> [([Char], [Char])]
 iterateList [x] rules = removeSimpleRules x rules (recursionNA (rules) (length (rules)) [x] [x])
 iterateList (x:xs) rules = removeSimpleRules x rules (recursionNA (rules) (length (rules)) [x] [x]) ++ iterateList xs rules
 
-
+-- Process final Algorithm 4.5 and change the data structure of context free grammar 
 processAlgorithm1 :: CFG_t -> CFG_t
 processAlgorithm1 (CFG_t nonterminal_symbols terminal_symbols starting_symbol grammar_rules) = CFG_t nonterminal_symbols terminal_symbols starting_symbol (iterateList (nonterminal_symbols) (grammar_rules ) )
--- FLP/proj1/proj1modules/Algorithm2.hs
+++ FLP/proj1/proj1modules/Algorithm2.hs
@@ -1,3 +1,10 @@
+-- Project name: bkg-2-cnf
+-- Author: Katerina Fortova
+-- Login: xforto00
+-- Year: 2020 / 2021
+
+-- Module description: Implementation of Algorithm 4.7 - Transformation to Chomsky Normal Form (CNF)
+
 module Algorithm2
   (validCNFrule, filterStep4, validStep4Rule, validStep5Rule,
   transformStep4Rule, checkChangedNonterminalsSubstr, removeChangedNonterminalBrackets,
@@ -17,6 +24,7 @@ import Parser
 import Types
 import Algorithm1
 
+-- Check whether rules in in CNF form (A -> BC or A -> a (Note: S -> epsilon is not included in assignment ))
 validCNFrule :: ([Char], [Char]) -> Bool
 validCNFrule rule
   -- A -> BC
@@ -25,51 +33,53 @@ validCNFrule rule
  | head (fst rule) `elem` ['A'..'Z'] && length (fst rule) == 1 && head (snd rule) `elem` ['a'..'z'] && length (snd rule) == 1 = True
  | otherwise = False
 
--- find rules for step4 (k > 2) which are not originally in cnf form
+-- Find rules for step4 of 4.7 algorithm (k > 2) which are not originally in cnf form
 filterStep4 :: [([Char], [Char])] -> [([Char], [Char])]
 filterStep4 rules = filter (\m -> length ( snd m) >  2) (filter (\n -> length  (snd n) >  2) rules)
 
+-- Check whether rule is in form for step 4 of 4.7 algorithm
 validStep4Rule :: ([Char], [Char]) -> Bool
 validStep4Rule rule
  | length ( snd rule) >  2  = True
  | otherwise = False
 
+-- Check whether rule is in form for step 5 of 4.7 algorithm (A -> bC, A -> Bc, A -> bc)
 validStep5Rule :: ([Char], [Char]) -> Bool
 validStep5Rule rule
  | length ( snd rule) ==  2  = True
  | otherwise = False
---transformStep4Rule rule [] counter = [] + [(fst ("A","bbbb"), take 1 (snd ("A","bbbb")) ++ "'<" ++ drop 1 (snd("A","bbbb"))  ++ ">")]
--- iterace od counter = delka bbbb (- 1) (jedno prvni pravidlo uz musi byt na zacatku pridane) (4 - 1 = 3)
--- rule je originalni pravidlo se kterym vstupujeme vzdy ("A", "bbbb")
+
+-- Do step4 of 4.7 algorithm and count final set with new rules
 transformStep4Rule :: ([Char], [Char]) -> [([Char], [Char])] -> Int -> [([Char], [Char])]
 transformStep4Rule rule [] counter = [] ++ [(fst rule, take 1 (snd rule) ++ "'<" ++ drop 1 (snd rule)  ++ ">")]
 transformStep4Rule rule new_added_rules 2 = new_added_rules
 transformStep4Rule rule new_added_rules counter = transformStep4Rule (rule) (new_added_rules ++ [((drop 2 (snd (last new_added_rules))), drop 1 (take 2 (drop 2 (snd (last new_added_rules))))  ++ "'<" ++ (drop 2 (drop 2 (snd (last new_added_rules)))))]) (counter - 1)
---transformStep4Rule rule new_added_rules counter = new_added_rules ++ ((drop 2 (snd (last [("A","b'<bbb>")]))), drop 1 (take 2 (drop 2 (snd (last [("A","b'<bbb>")]))))  ++ "'<" ++ (drop 2 (drop 2 (snd (last [("A","b'<bbb>")])))))
 
+-- Check whether nonterminal is in list of nonterms
 checkChangedNonterminalsSubstr :: [String] -> ([Char], [Char]) -> [Bool]
 checkChangedNonterminalsSubstr [x] rule = [(isInfixOf x (snd rule))]
 checkChangedNonterminalsSubstr (x:xs) rule = [(isInfixOf x (snd rule))] ++ checkChangedNonterminalsSubstr xs rule
 
+-- Remove some characters from rule in specific cases
 removeChangedNonterminalBrackets :: String -> String
 removeChangedNonterminalBrackets xs = [ x | x <- xs, not (x `elem` "<>") ]
 
 removeChangedNonterminalComma :: String -> String
 removeChangedNonterminalComma xs = [ x | x <- xs, not (x `elem` "'") ]
 
--- remove <A> to A
+-- Remove <A> to A
 checkChangedNonterminals1 :: [[Char]] -> ([Char], [Char]) -> ([Char], [Char])
 checkChangedNonterminals1 nonterminals rule
  | (all (==False) (checkChangedNonterminalsSubstr (map (\n -> "<" ++ n ++ ">") nonterminals) (rule))) = rule
  | otherwise = ((fst rule), removeChangedNonterminalBrackets (snd rule) )
 
--- remove A' to A
+-- Remove A' to A
 checkChangedNonterminals2 :: [[Char]] -> ([Char], [Char]) -> ([Char], [Char])
 checkChangedNonterminals2 nonterminals rule
  | (all (==False) (checkChangedNonterminalsSubstr (map (\n -> n ++ "'") nonterminals) (rule))) = rule
  | otherwise = ((fst rule), removeChangedNonterminalComma(snd rule) )
 
--- remove <a> to a'
+-- Remove <a> to a'
 checkChangedTerminals :: [[Char]] -> ([Char], [Char]) -> ([Char], [Char])
 checkChangedTerminals terminals rule
  | (all (==False) (checkChangedNonterminalsSubstr (map (\n -> "<" ++ n ++ ">") terminals) (rule))) = rule
@@ -84,6 +94,7 @@ checkChangedNonterminals3 nonterminals rule
  | drop 2 (snd rule) `elem` (map (\n -> n ++ "'") nonterminals)  = (fst rule, take 3 (snd rule))
  | otherwise = rule
 
+-- Get new rules for each new nonterm (a' -> a)
 newRulesStep6 :: [[Char]] -> ([Char], [Char]) -> [([Char], [Char])]
 newRulesStep6 terminals rule
  | take 2 (snd rule) `elem` (map (\n -> n ++ "'") terminals) && drop 2 (snd rule) `elem` (map (\n -> n ++ "'") terminals) = [(take 2 (snd rule), take 1 (snd rule)), (drop 2 (snd rule), take 1 (drop 2 (snd rule)))]
@@ -96,10 +107,12 @@ newRulesStep6 terminals rule
 transformStep5Rule :: ([Char], [Char]) -> ([Char], [Char])
 transformStep5Rule rule = ((fst rule), take 1 (snd (rule)) ++ "'" ++ drop 1 (snd (rule)) ++  "'" )
 
+-- Loop through all terminals, find new terminals and add their rules (a' -> a)
 transformStep6Rule :: [([Char], [Char])] -> [String] -> [([Char], [Char])]
 transformStep6Rule [x] terminals = newRulesStep6 terminals x
 transformStep6Rule (x:xs) terminals = newRulesStep6 terminals x ++ transformStep6Rule xs terminals
 
+-- Generate new CNF rules based on their format, 4.7 algorithm to step 5, step 6 is processed separately
 newCNFRules ::  [([Char], [Char])] -> [String] -> [String] -> [([Char], [Char])]
 newCNFRules [x] nonterminals terminals
  | validCNFrule x == True = [x]
@@ -111,18 +124,20 @@ newCNFRules (x:xs) nonterminals terminals
  | validCNFrule x == False && validStep4Rule x == True =  map (checkChangedTerminals terminals) (map (checkChangedNonterminals2 nonterminals) (map (checkChangedNonterminals1 nonterminals) (transformStep4Rule x ([] ++ [(fst x, take 1 (snd x) ++ "'<" ++ drop 1 (snd x)  ++ ">")]) (length (snd x))))) ++ newCNFRules xs nonterminals terminals
  | validCNFrule x == False && validStep5Rule x == True = [checkChangedNonterminals3 (nonterminals) (transformStep5Rule x)] ++ newCNFRules xs nonterminals terminals
  | otherwise = newCNFRules xs nonterminals terminals
--- | validCNFrule == False && validStep4Rule == True =
 
--- final rules with Step 6 a' -> a rules
+-- Final rules with Step 6 a' -> a rules
 allCNFRules :: [([Char], [Char])] -> [String] -> [String] -> [([Char], [Char])]
 allCNFRules rules nonterminals terminals = nub (newCNFRules rules nonterminals terminals ++ transformStep6Rule (newCNFRules rules nonterminals terminals) (terminals))
 
+-- Add new generated nonterms in CNF rules to nonterms set
 updateNontermsSet :: [([Char], [Char])] -> [String] -> [String] -> [[Char]]
 updateNontermsSet rules nonterminals terminals = nub (map fst (allCNFRules rules nonterminals terminals) ++ nonterminals)
 
+-- Finally process 4.7 algorithm
 processAlgorithm2 :: CFG_t -> CFG_t
 processAlgorithm2 (CFG_t nonterminal_symbols terminal_symbols starting_symbol grammar_rules) = CFG_t (updateNontermsSet grammar_rules nonterminal_symbols terminal_symbols) terminal_symbols starting_symbol (allCNFRules grammar_rules nonterminal_symbols terminal_symbols)
 
+-- Processed 4.5, 4.7 algorithm based on arguments -1 or -2 from user 
 processAlgorithms :: String -> CFG_t -> CFG_t
 processAlgorithms "-1" grammar_input = processAlgorithm1 grammar_input
 processAlgorithms "-2" grammar_input = processAlgorithm2 (processAlgorithm1 grammar_input)
