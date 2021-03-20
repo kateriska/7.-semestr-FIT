32e9f90b3ac568d5f2c7cc64303a4b7917f6e4eb
Kateřina Fořtová <43783793+kateriska@users.noreply.github.com>
2021-03-20 11:48:26 +0100

    first trying vol 2

--- PRL/proj1/pms.cpp
+++ PRL/proj1/pms.cpp
@@ -26,6 +26,7 @@ int main(int argc, char *argv[])
    MPI_Status stat;            //struct- obsahuje kod- source, tag, error
 
    queue<int16_t> input_queue;
+   int numbers_count = 0;
 
    //MPI INIT
    MPI_Init(&argc,&argv);                          // inicializace MPI
@@ -47,25 +48,62 @@ int main(int argc, char *argv[])
            number= fin.get();
            if(!fin.good()) break;                      //nacte i eof, takze vyskocim
            cout<<invar<<":"<<number<<endl;             //kdo dostane kere cislo
+           numbers_count++;
            input_queue.push(number);
-           cout << "The input queue is : ";
-           showq(input_queue);
-           //MPI_Send(&number, 1, MPI_INT, invar + 1, TAG, MPI_COMM_WORLD); //buffer,velikost,typ,rank prijemce,tag,komunikacni skupina
+           //cout << "The input queue is : ";
+           //showq(input_queue);
+           //MPI_Send(&number, 1, MPI_INT, myid + 1, TAG, MPI_COMM_WORLD); //buffer,velikost,typ,rank prijemce,tag,komunikacni skupina
            invar++;
        }//while
+       //showq(input_queue);
+       cout << numbers_count;
        fin.close();
    }//nacteni souboru
 
+   int k = 0;
+   queue<int16_t> first_merging_queue;
+   queue<int16_t> second_merging_queue;
+   bool add_to_first_merging_queue = 1;
+   int received_number;
+   while (k <= 32){
    if (myid == 0)
    {
      if (input_queue.empty() == 0)
      {
-       int first_number = input_queue.front();
+       int number = input_queue.front();
+       cout << number;
        input_queue.pop();
-       MPI_Send(&first_number, 1, MPI_INT, myid + 1, TAG, MPI_COMM_WORLD);
-       cout << "CPU id: " << myid + 1;
+       MPI_Send(&number, 1, MPI_INT, myid + 1, TAG, MPI_COMM_WORLD);
+       //cout << "CPU id: " << myid + 1;
      }
    }
+   else if (myid == 1)
+   {
+     MPI_Recv(&received_number, 1, MPI_INT, myid - 1, TAG, MPI_COMM_WORLD, &stat);
+     cout << received_number;
+     /*
+     if (add_to_first_merging_queue == 1)
+     {
+       first_merging_queue.push(received_number);
+       add_to_first_merging_queue = 0;
+     }
+     else
+     {
+       second_merging_queue.push(received_number);
+       add_to_first_merging_queue = 1;
+     }
+     */
+   }
+
+   k++;
+ }
+
+ showq(first_merging_queue);
+ showq(second_merging_queue);
+
+
+
+
 
    MPI_Finalize();
    return 0;
