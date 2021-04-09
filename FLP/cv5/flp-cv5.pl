5b53e48d6bf7f2f2e3e49900b156d2991c1e429d
Kateřina Fořtová <43783793+kateriska@users.noreply.github.com>
2021-04-09 15:06:38 +0200

    Add files via upload

--- FLP/cv5/flp-cv5.pl
+++ FLP/cv5/flp-cv5.pl
@@ -36,7 +36,12 @@ inkrementuj(X,Y) :- Y is X+1.
 dekrementuj(X,Y) :- Y is X-1.
 doleva(I) :- pohni(I, dekrementuj).
 doprava(I) :- pohni(I, inkrementuj).
-pohni(I, Operace) :- call(Operace, I).
+% robot se posune, pokud misto kam se chce posunout neni obsazeno 
+pohni(I, Operace) :- Operace = 'doleva', robot(I,P), dekrementuj(P,Y), not(obsazeno(Y)), odstran(P), vytvor(I,Y).
+pohni(I, Operace) :- Operace = 'doprava', robot(I,P), inkrementuj(P,Y), not(obsazeno(Y)), odstran(P), vytvor(I,Y).
+% robot spadne do diry a zmizi
+pohni(I, Operace) :- Operace = 'doleva', robot(I,P), dekrementuj(P,Y), dira(Y), odstran(P).
+pohni(I, Operace) :- Operace = 'doprava', robot(I,P), inkrementuj(P,Y), dira(Y), odstran(P).
 
 armageddon :- obsazene_roboty(X), forall(member(P, X), vybuch(P)).
 vybuch(P) :- odstran(P), vytvor(P).
@@ -51,11 +56,11 @@ vybuch(P) :- odstran(P), vytvor(P).
 
 g_size(3).
 
-/*
-g_test(X:Y) :-
-*/
 
-/*
+g_test(X:Y) :- X > 0, X < 4, Y > 0, Y < 4.
+
+
+
 g_move(X1:Y1, X2:Y2) :- X2 is X1 - 1, Y2 is Y1 - 1, g_test(X2:Y2).
 g_move(X1:Y1, X2:Y2) :- X2 is X1 - 1, Y2 is Y1 + 0, g_test(X2:Y2).
 g_move(X1:Y1, X2:Y2) :- X2 is X1 - 1, Y2 is Y1 + 1, g_test(X2:Y2).
@@ -64,12 +69,13 @@ g_move(X1:Y1, X2:Y2) :- X2 is X1 + 0, Y2 is Y1 + 1, g_test(X2:Y2).
 g_move(X1:Y1, X2:Y2) :- X2 is X1 + 1, Y2 is Y1 - 1, g_test(X2:Y2).
 g_move(X1:Y1, X2:Y2) :- X2 is X1 + 1, Y2 is Y1 + 0, g_test(X2:Y2).
 g_move(X1:Y1, X2:Y2) :- X2 is X1 + 1, Y2 is Y1 + 1, g_test(X2:Y2).
-*/
 
-/*
-g_one(X:Y, Len, L, R) :-
-g_one(X:Y, Len, L, R) :-
-*/
+g_one(X:Y, Len, L, Res) :- reverse([X:Y|L], Res), length(Res, Len).
+g_one(X:Y, Len, L, Res) :- g_move(X:Y, Xn:Yn), \+ memberchk(Xn:Yn, L), g_one(Xn:Yn, Len, [X:Y|L], Res).
+
+%g_one(X:Y, Len, L, R) :-
+%g_one(X:Y, Len, L, R) :-
+
 
 /*
 g_all(R, Len) :-
