4ae9b668d08a42edc1f9b7c1cefa1bda9b8bc066
Kateřina Fořtová <43783793+kateriska@users.noreply.github.com>
2021-04-10 15:31:15 +0200

    loop based on json not char files
    Now it is looped by json objects and not char files - it is more simple with operating with filtered json. Json data from specific orgin (e.g. rcam) are filtered and only some of data are saved to filtered json based on seed, because there is a huge amount of rcam multipliers - algorithm is more faster.

--- BIN/proj/countFeatures.py
+++ BIN/proj/countFeatures.py
@@ -5,18 +5,12 @@ import os
 import orjson
 from matplotlib import pyplot as plt
 import scipy.stats
+import time
 
-chr_path = './cgp-approx14ep.chr/*'
+start_time = time.time()
+
+chr_path = './cgp-approx14ep.chr/'
 csv_path = './csvFiles/chrFeatures.csv'
-'''
-for file in glob.glob(chr_path):
-    f_in = open(file, 'rb')
-    file_substr = file.split('/')[-1] # get name of processed file
-    f_out = gzip.open("./compressedChrFiles/" + file_substr + '.zip', 'wb')
-    f_out.writelines(f_in)
-    f_out.close()
-    f_in.close()
-'''
 
 xor_values = []
 mae_values = []
@@ -30,196 +24,22 @@ pwr_values = []
 xor_xor_xor_values = []
 and_xor_and_count_values = []
 
+# write head of csv file
 with open(csv_path, 'w+') as csv_file:
     writer = csv.writer(csv_file)
     writer.writerow(["Name", "IDA Count", "INVA Count", "AND2 Count", "OR2 Count", "XOR2 Count", "NAND2 Count", "NOR2 Count", "XNOR2 Count", "Count of Used Cells", "Compressed Chr Size", "a XOR b XOR c XOR d Count", "(a AND b) XOR (c AND d) Count"])
 
 csv_file.close()
 
-for file in glob.glob(chr_path):
-    f_in = open(file, 'rb')
-    file_substr = file.split('/')[-1] # get name of processed file
-    if ("_rcam" not in file_substr):
-        continue
-    file_substr = file_substr[:-4] # cut .char
-    f_out = gzip.open("./compressedChrFiles/" + file_substr + '.zip', 'wb')
-    f_out.writelines(f_in)
-    f_out.close()
-    f_in.close()
-
-    compressed_file_size = os.path.getsize("./compressedChrFiles/" + file_substr + '.zip')
-
-    '''
-    Kódy použitých hradel (mapování v CGP s 2-
-    vstupovými uzly)
-    0: IDA (drat)
-    1: INVA (inverze prvniho stupu)
-    2: AND2
-    3: OR2
-    4: XOR2
-    5: NAND2
-    6: NOR2
-    7: XNOR2
-    '''
-    ida_count = 0
-    inva_count = 0
-    and_count = 0
-    or_count = 0
-    xor_count = 0
-    nand_count = 0
-    nor_count = 0
-    xnor_count = 0
-
-    #file_substr = file.split('/')[-1] # get name of processed file
-    print(file_substr)
-
-    readed_file = open(file, 'r')
-    lines = readed_file.readlines()
-
-    count = 0
-    # Strips the newline character
-    for line in lines:
-        count += 1
-        if (line[0:1] == "#"):
+# iterate through json file
+with open('filtered_data.json', "rb") as json_file:
+    json_data = orjson.loads(json_file.read())
+    for file_substr in json_data:
+        if ("_rcam" not in file_substr):
             continue
-        #print(line.strip())
-        cgp_gates_codes = line[line.find('}') + 2:len(line)]
-        #print(cgp_gates_codes)
-        outputs = (cgp_gates_codes[cgp_gates_codes.rfind('(') + 1:-1]).split(',')
-        outputs_list = list (map(int, outputs))
-        cgp_gates_codes_list = cgp_gates_codes.split(')(')
-        cgp_gates_codes_list = cgp_gates_codes_list[:-1]
-        #print (cgp_gates_codes_list)
-        #print(outputs_list)
 
-    used_cgp_gates_codes_list = []
-    for cgp_gates_code in cgp_gates_codes_list:
-        gate_id = int (cgp_gates_code[1:cgp_gates_code.find(']')])
-        #print(gate_id)
-        input1 = int (cgp_gates_code[cgp_gates_code.find(']') + 1:cgp_gates_code.find(',')])
-        #print(input1)
-        input2 = int (cgp_gates_code[cgp_gates_code.find(',') + 1:cgp_gates_code.rfind(',')])
-        #print(input2)
-
-        for cgp_gates_code2 in cgp_gates_codes_list:
-            if (cgp_gates_code == cgp_gates_code2):
-                continue
-            gate_id2 = int (cgp_gates_code2[1:cgp_gates_code2.find(']')])
-            input12 = int (cgp_gates_code2[cgp_gates_code2.find(']') + 1:cgp_gates_code2.find(',')])
-            input22 = int (cgp_gates_code2[cgp_gates_code2.find(',') + 1:cgp_gates_code2.rfind(',')])
-
-            if (input12 == gate_id or input22 == gate_id or gate_id in outputs_list):
-                used_cgp_gates_codes_list.append(cgp_gates_code)
-                break
-    used_cells_size = len(used_cgp_gates_codes_list)
-    #print("Size of used cells: " + str(len(used_cgp_gates_codes_list)))
-
-    xor_cells = []
-    and_cells = []
-    for cgp_gates_code in used_cgp_gates_codes_list:
-        used_gate_id_str = cgp_gates_code[cgp_gates_code.rfind(',')+1:len(cgp_gates_code)]
-        #print(used_gate_id_str)
-
-        used_gate_id = int (used_gate_id_str)
-        #print (used_gate_id)
-
-        if (used_gate_id == 0):
-            ida_count += 1
-        elif (used_gate_id == 1):
-            inva_count += 1
-        elif (used_gate_id == 2):
-            and_count += 1
-            and_cells.append(cgp_gates_code)
-        elif (used_gate_id == 3):
-            or_count += 1
-        elif (used_gate_id == 4):
-            xor_count += 1
-            xor_cells.append(cgp_gates_code)
-        elif (used_gate_id == 5):
-            nand_count += 1
-        elif (used_gate_id == 6):
-            nor_count += 1
-        elif (used_gate_id == 7):
-            xnor_count += 1
-
-
-    xor_xor_xor_count = 0
-    for xor_cell in xor_cells:
-        gate_id = int (xor_cell[1:xor_cell.find(']')])
-        #print(gate_id)
-        input1 = int (xor_cell[xor_cell.find(']') + 1:xor_cell.find(',')])
-        #print(input1)
-        input2 = int (xor_cell[xor_cell.find(',') + 1:xor_cell.rfind(',')])
-        #print(input2)
-
-        for xor_cell2 in xor_cells:
-            if (xor_cell == xor_cell2):
-                continue
-            gate_id2 = int (xor_cell2[1:xor_cell2.find(']')])
-            input12 = int (xor_cell2[xor_cell2.find(']') + 1:xor_cell2.find(',')])
-            input22 = int (xor_cell2[xor_cell2.find(',') + 1:xor_cell2.rfind(',')])
-
-            if (gate_id == input12 or gate_id == input22): # a XOR b XOR c
-                for xor_cell3 in xor_cells:
-                    if (xor_cell2 == xor_cell3):
-                        continue
-                    gate_id3 = int (xor_cell3[1:xor_cell3.find(']')])
-                    input13 = int (xor_cell3[xor_cell3.find(']') + 1:xor_cell3.find(',')])
-                    input23 = int (xor_cell3[xor_cell3.find(',') + 1:xor_cell3.rfind(',')])
-
-                    if (gate_id2 == input13 or gate_id2 == input23):
-                        #print(xor_cell + "->" + xor_cell2 + "->" +xor_cell3)
-                        xor_xor_xor_count += 1
-    #print("XOR XOR XOR count " + str(xor_xor_xor_count))
-
-
-    and_xor_and_count = 0
-    for and_cell in and_cells:
-        gate_id = int (and_cell[1:and_cell.find(']')])
-        #print(gate_id)
-        input1 = int (and_cell[and_cell.find(']') + 1:and_cell.find(',')])
-        #print(input1)
-        input2 = int (and_cell[and_cell.find(',') + 1:and_cell.rfind(',')])
-        #print(input2)
-
-        for xor_cell2 in xor_cells:
-            gate_id2 = int (xor_cell2[1:xor_cell2.find(']')])
-            input12 = int (xor_cell2[xor_cell2.find(']') + 1:xor_cell2.find(',')])
-            input22 = int (xor_cell2[xor_cell2.find(',') + 1:xor_cell2.rfind(',')])
-
-            if (gate_id == input12 or gate_id == input22): # a AND b XOR
-                for and_cell3 in and_cells:
-                    if (and_cell == and_cell3):
-                        continue
-                    gate_id3 = int (and_cell3[1:and_cell3.find(']')])
-                    input13 = int (and_cell3[and_cell3.find(']') + 1:and_cell3.find(',')])
-                    input23 = int (and_cell3[and_cell3.find(',') + 1:and_cell3.rfind(',')])
-
-                    if (gate_id3 == input12 or gate_id3 == input22):
-                        #print(and_cell + "->" + xor_cell2 + "->" +and_cell3)
-                        and_xor_and_count += 1
-    #print("AND XOR AND count " + str(and_xor_and_count))
-
-    xor_values.append(xor_count)
-    compressed_chr_sizes.append(compressed_file_size)
-    xor_xor_xor_values.append(xor_xor_xor_count)
-    and_xor_and_count_values.append(and_xor_and_count)
-
-    with open('./csvFiles/chrFeatures.csv', 'a', newline='') as csv_file:
-        writer = csv.writer(csv_file)
-        writer.writerow([file_substr, ida_count, inva_count, and_count, or_count, xor_count, nand_count, nor_count, xnor_count, used_cells_size, compressed_file_size, xor_xor_xor_count, and_xor_and_count])
-
-    csv_file.close()
-
-    with open('filtered_data.json', "rb") as json_file:
-        json_data = orjson.loads(json_file.read())
-
-        #json_name_id = json_data.keys()
-        #print (json_name_id)
-
-
+        # extract metrics from json file
         mae_value = json_data[file_substr][0]["mae"]
-        #print(mae_value)
         mae_values.append(mae_value)
         wce_value = json_data[file_substr][0]["wce"]
         wce_values.append(wce_value)
@@ -239,6 +59,173 @@ for file in glob.glob(chr_path):
         levels_value = json_data[file_substr][0]["levels"]
         levels_values.append(levels_value)
 
+        # open chr file of json object
+        f_in = open(chr_path + file_substr + ".chr", 'rb')
+
+        # create zip of chr file and compute its size
+        f_out = gzip.open("./compressedChrFiles/" + file_substr + '.zip', 'wb')
+        f_out.writelines(f_in)
+        f_out.close()
+        f_in.close()
+
+        compressed_file_size = os.path.getsize("./compressedChrFiles/" + file_substr + '.zip')
+
+        '''
+        Kódy použitých hradel (mapování v CGP s 2-
+        vstupovými uzly)
+        0: IDA (drat)
+        1: INVA (inverze prvniho stupu)
+        2: AND2
+        3: OR2
+        4: XOR2
+        5: NAND2
+        6: NOR2
+        7: XNOR2
+        '''
+        ida_count = 0
+        inva_count = 0
+        and_count = 0
+        or_count = 0
+        xor_count = 0
+        nand_count = 0
+        nor_count = 0
+        xnor_count = 0
+
+        #file_substr = file.split('/')[-1] # get name of processed file
+        print(file_substr)
+
+        # open chr file for computing features based on chr file
+        readed_file = open(chr_path + file_substr + ".chr", 'r')
+        lines = readed_file.readlines()
+
+        count = 0
+        # Strips the newline character
+        for line in lines:
+            count += 1
+            if (line[0:1] == "#"):
+                continue
+            cgp_gates_codes = line[line.find('}') + 2:len(line)]
+            outputs = (cgp_gates_codes[cgp_gates_codes.rfind('(') + 1:-1]).split(',')
+            outputs_list = list (map(int, outputs))
+            cgp_gates_codes_list = cgp_gates_codes.split(')(')
+            cgp_gates_codes_list = cgp_gates_codes_list[:-1]
+
+        # find used cells by cgp
+        used_cgp_gates_codes_list = []
+        for cgp_gates_code in cgp_gates_codes_list:
+            gate_id = int (cgp_gates_code[1:cgp_gates_code.find(']')])
+            input1 = int (cgp_gates_code[cgp_gates_code.find(']') + 1:cgp_gates_code.find(',')])
+            input2 = int (cgp_gates_code[cgp_gates_code.find(',') + 1:cgp_gates_code.rfind(',')])
+
+            for cgp_gates_code2 in cgp_gates_codes_list:
+                if (cgp_gates_code == cgp_gates_code2):
+                    continue
+                gate_id2 = int (cgp_gates_code2[1:cgp_gates_code2.find(']')])
+                input12 = int (cgp_gates_code2[cgp_gates_code2.find(']') + 1:cgp_gates_code2.find(',')])
+                input22 = int (cgp_gates_code2[cgp_gates_code2.find(',') + 1:cgp_gates_code2.rfind(',')])
+
+                if (input12 == gate_id or input22 == gate_id or gate_id in outputs_list):
+                    used_cgp_gates_codes_list.append(cgp_gates_code)
+                    break
+
+        used_cells_size = len(used_cgp_gates_codes_list)
+        #print("Size of used cells: " + str(len(used_cgp_gates_codes_list)))
+
+        xor_cells = []
+        and_cells = []
+
+        # find count of used types of cells
+        for cgp_gates_code in used_cgp_gates_codes_list:
+            used_gate_id_str = cgp_gates_code[cgp_gates_code.rfind(',')+1:len(cgp_gates_code)]
+            used_gate_id = int (used_gate_id_str)
+
+            if (used_gate_id == 0):
+                ida_count += 1
+            elif (used_gate_id == 1):
+                inva_count += 1
+            elif (used_gate_id == 2):
+                and_count += 1
+                and_cells.append(cgp_gates_code)
+            elif (used_gate_id == 3):
+                or_count += 1
+            elif (used_gate_id == 4):
+                xor_count += 1
+                xor_cells.append(cgp_gates_code)
+            elif (used_gate_id == 5):
+                nand_count += 1
+            elif (used_gate_id == 6):
+                nor_count += 1
+            elif (used_gate_id == 7):
+                xnor_count += 1
+
+        # count of subparts a XOR b XOR c XOR d
+        xor_xor_xor_count = 0
+        for xor_cell in xor_cells:
+            gate_id = int (xor_cell[1:xor_cell.find(']')])
+            input1 = int (xor_cell[xor_cell.find(']') + 1:xor_cell.find(',')])
+            input2 = int (xor_cell[xor_cell.find(',') + 1:xor_cell.rfind(',')])
+
+            for xor_cell2 in xor_cells:
+                if (xor_cell == xor_cell2):
+                    continue
+                gate_id2 = int (xor_cell2[1:xor_cell2.find(']')])
+                input12 = int (xor_cell2[xor_cell2.find(']') + 1:xor_cell2.find(',')])
+                input22 = int (xor_cell2[xor_cell2.find(',') + 1:xor_cell2.rfind(',')])
+
+                if (gate_id == input12 or gate_id == input22): # a XOR b XOR c
+                    for xor_cell3 in xor_cells:
+                        if (xor_cell2 == xor_cell3):
+                            continue
+                        gate_id3 = int (xor_cell3[1:xor_cell3.find(']')])
+                        input13 = int (xor_cell3[xor_cell3.find(']') + 1:xor_cell3.find(',')])
+                        input23 = int (xor_cell3[xor_cell3.find(',') + 1:xor_cell3.rfind(',')])
+
+                        if (gate_id2 == input13 or gate_id2 == input23):
+                            #print(xor_cell + "->" + xor_cell2 + "->" +xor_cell3)
+                            xor_xor_xor_count += 1
+        #print("XOR XOR XOR count " + str(xor_xor_xor_count))
+
+        # count of subparts (a AND b) XOR (c AND d)
+        and_xor_and_count = 0
+        for and_cell in and_cells:
+            gate_id = int (and_cell[1:and_cell.find(']')])
+            input1 = int (and_cell[and_cell.find(']') + 1:and_cell.find(',')])
+            input2 = int (and_cell[and_cell.find(',') + 1:and_cell.rfind(',')])
+
+            for xor_cell2 in xor_cells:
+                gate_id2 = int (xor_cell2[1:xor_cell2.find(']')])
+                input12 = int (xor_cell2[xor_cell2.find(']') + 1:xor_cell2.find(',')])
+                input22 = int (xor_cell2[xor_cell2.find(',') + 1:xor_cell2.rfind(',')])
+
+                if (gate_id == input12 or gate_id == input22): # a AND b XOR
+                    for and_cell3 in and_cells:
+                        if (and_cell == and_cell3):
+                            continue
+                        gate_id3 = int (and_cell3[1:and_cell3.find(']')])
+                        input13 = int (and_cell3[and_cell3.find(']') + 1:and_cell3.find(',')])
+                        input23 = int (and_cell3[and_cell3.find(',') + 1:and_cell3.rfind(',')])
+
+                        if (gate_id3 == input12 or gate_id3 == input22):
+                            #print(and_cell + "->" + xor_cell2 + "->" +and_cell3)
+                            and_xor_and_count += 1
+        #print("AND XOR AND count " + str(and_xor_and_count))
+
+        xor_values.append(xor_count)
+        compressed_chr_sizes.append(compressed_file_size)
+        xor_xor_xor_values.append(xor_xor_xor_count)
+        and_xor_and_count_values.append(and_xor_and_count)
+
+        # write info to csv file
+        with open('./csvFiles/chrFeatures.csv', 'a', newline='') as csv_file:
+            writer = csv.writer(csv_file)
+            writer.writerow([file_substr, ida_count, inva_count, and_count, or_count, xor_count, nand_count, nor_count, xnor_count, used_cells_size, compressed_file_size, xor_xor_xor_count, and_xor_and_count])
+
+        csv_file.close()
+
+# print duration of program
+print("--- %s seconds ---" % (time.time() - start_time))
+
+# show some interesting graphs and correlations of dependecies 
 figure = plt.figure(figsize=(30, 30))
 xor_mae_plot = figure.add_subplot(4,3,1)
 xor_mae_plot.scatter(xor_values, mae_values)
--- BIN/proj/filterJson.py
+++ BIN/proj/filterJson.py
@@ -1,12 +1,16 @@
+from random import seed
+from random import random
 import json
 
+seed(1)
 with open('cgp-approx14ep.json', "r") as json_file:
     json_data = json.load(json_file)
 
     data = {}
+    i = 0
     for item in json_data:
         print(item)
-        if ("_rcam" not in item):
+        if ("_rcam" not in item or random() < 0.5):
             continue
         data[item] = []
         data[item].append({
@@ -20,18 +24,9 @@ with open('cgp-approx14ep.json', "r") as json_file:
             "pdk45_pwr": json_data[item]["pdk45_pwr"]
 })
         #print(data)
+        i = i + 1
 
     with open('filtered_data.json', 'w+') as outfile:
         json.dump(data, outfile)
         #print(json_data['mult8_cgp14ep_ep65536_wc9_wtmrca']["mae"])
-
-
-
-with open('filtered_data.json', "r") as json_file:
-    json_data = json.load(json_file)
-
-    #json_name_id = json_data.keys()
-    #print (json_name_id)
-
-
-    #print(json_data['mult8_cgp14ep_ep65536_wc9_wtmrca'][0]["mae"])
+print("Count of objects in filtered json file: " + str(i))
