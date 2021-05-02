bef2e673705d902742895b3dc47c08ff5cd48e14
Kateřina Fořtová <43783793+kateriska@users.noreply.github.com>
2021-05-02 14:39:57 +0200

    updated model of network

--- BIN/proj/clasifyOrigin.py
+++ BIN/proj/clasifyOrigin.py
@@ -21,13 +21,14 @@ class Net(nn.Module):
     def __init__(self,D_in,H,D_out):
         super(Net,self).__init__()
 
-        self.layer_1 = nn.Linear(D_in, 512)
-        self.layer_2 = nn.Linear(512, 128)
+        #self.layer_1 = nn.Linear(D_in, 512)
+        self.layer_2 = nn.Linear(D_in, 128)
         self.layer_3 = nn.Linear(128, 64)
         self.layer_out = nn.Linear(64, D_out)
 
         self.relu = nn.ReLU()
-        self.dropout = nn.Dropout(p=0.2)
+        self.softmax = nn.Softmax(dim=1)
+        self.dropout = nn.Dropout(p=0.5)
         self.batchnorm1 = nn.BatchNorm1d(512)
         self.batchnorm2 = nn.BatchNorm1d(128)
         self.batchnorm3 = nn.BatchNorm1d(64)
@@ -38,19 +39,20 @@ class Net(nn.Module):
         #x = torch.nn.functional.softmax (self.linear1(x))
         #x = self.linear2(x)
         #return x
-
+        '''
         x = self.layer_1(x)
         x = self.batchnorm1(x)
         x = self.relu(x)
+        '''
 
         x = self.layer_2(x)
-        x = self.batchnorm2(x)
+        #x = self.batchnorm2(x)
         x = self.relu(x)
         x = self.dropout(x)
 
         x = self.layer_3(x)
-        x = self.batchnorm3(x)
-        x = self.relu(x)
+        #x = self.batchnorm3(x)
+        x = self.softmax(x)
         x = self.dropout(x)
 
         x = self.layer_out(x)
@@ -191,7 +193,7 @@ criterion = nn.CrossEntropyLoss()
 learning_rate = 0.001
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
-n_epochs = 2
+n_epochs = 1000
 loss_list = []
 
 epochs_list = []
