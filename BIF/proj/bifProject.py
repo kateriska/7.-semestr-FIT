21f04632980539271e49107b8e38f631d9075b91
Kateřina Fořtová <43783793+kateriska@users.noreply.github.com>
2021-05-03 19:16:14 +0200

    code clean

--- BIF/proj/bifProject.py
+++ BIF/proj/bifProject.py
@@ -2,7 +2,17 @@ from Bio import Phylo
 from Bio import SeqIO
 import csv
 
-# https://www.geeksforgeeks.org/python-program-to-convert-a-list-to-string/
+'''
+function for converting list into string
+Source:
+***************************************************************************************
+  *    Title: Python program to convert a list to string
+  *    Author: author of geeksforgeeks with nickname "Shivam_k" -> https://auth.geeksforgeeks.org/user/Shivam_k/articles
+  *    Date: 4.11.2019
+  *    Code version: 1
+  *    Availability: https://www.geeksforgeeks.org/python-program-to-convert-a-list-to-string/
+**************************************************************************************
+'''
 def listToString(s):
     # initialize an empty string
     str1 = ""
@@ -14,11 +24,31 @@ def listToString(s):
     # return string
     return str1
 
-#https://www.geeksforgeeks.org/python-split-string-into-list-of-characters/
+'''
+function for converting string into list of chars
+Source:
+***************************************************************************************
+  *    Title: Python | Split string into list of characters
+  *    Author: author of geeksforgeeks with nickname "Smitha Dinesh Semwal" -> https://auth.geeksforgeeks.org/user/Smitha%20Dinesh%20Semwal/articles
+  *    Date: 19.2.2021
+  *    Code version: 1
+  *    Availability: https://www.geeksforgeeks.org/python-split-string-into-list-of-characters/
+**************************************************************************************
+'''
 def split(word):
     return [char for char in word]
 
-# https://biopython.org/wiki/Phylo_cookbook
+'''
+function for finding parents of clades hierarchically
+Source:
+***************************************************************************************
+  *    Title: Bio.Phylo Cookbook.
+  *    Authors: users of github with nicknames "etal", "peterjc", "MarkusPiotrowski", "niemasd", "AstrobioMike", "hlapp", "Gasta88"
+  *    Date: 16.8.2020
+  *    Code version: 1
+  *    Availability: https://biopython.org/wiki/Phylo_cookbook, https://github.com/biopython/biopython.github.io/blob/master/wiki/Phylo_cookbook.md
+**************************************************************************************
+'''
 def all_parents(tree):
     parents = {}
     for clade in tree.find_clades(order="level"):
@@ -28,21 +58,15 @@ def all_parents(tree):
 
 # find recursively all parent nodes ids (confidence) from sequences in fasta file (e.g. from sequence XP_007318498.1) and branch length of specific object
 def findAllParentsRecursively(parents, child_key, found_parents, branch_lengths_to_nodes):
-    #print(child_key.confidence)
     found_parent = parents.get(child_key)
 
     if found_parent is None:
         return found_parents, branch_lengths_to_nodes
 
-    #print(found_parent)
-    #print(found_parent.confidence)
     found_parents.append(int(found_parent.confidence))
 
     if (found_parent.branch_length is not None):
         branch_lengths_to_nodes.append(float(found_parent.branch_length))
-    #else:
-    #    branch_lengths_to_nodes.append(0)
-    #print(found_parents)
 
     return (findAllParentsRecursively(parents, found_parent, found_parents, branch_lengths_to_nodes))
 
@@ -54,7 +78,6 @@ def getMostProbableAminoacids():
     with open("./files/ancestrals.csv", "r") as a_file:
       for line in a_file:
         line = line.strip()
-        #print(line)
 
         # skip header of file
         if (count == 0):
@@ -62,7 +85,6 @@ def getMostProbableAminoacids():
             continue
 
         line_list = line.split(",")
-        #print(line_list)
 
         node_key = int (line_list[0])
         ancestrals_dict[node_key] = []
@@ -87,19 +109,15 @@ def getMostProbableAminoacids():
         node_key = int (line_list[0])
         index = int (line_list[1]) - 1 # positions are indexed from 1
 
-        #print(line_list)
         # get all probabilities for each aminoacid
         aa_probabilities = line_list[2:]
-        #print(aa_probabilities)
 
         max_probability = 0
         max_aa_index = 0
         aa_index = 0
 
-        #print(aa_probabilities)
         # get maximum probability of aminoacid of each index
         for p in aa_probabilities:
-            #print(p)
             if (p == '-'):
                 aa_index = aa_index + 1
                 continue
@@ -111,12 +129,6 @@ def getMostProbableAminoacids():
 
             aa_index = aa_index + 1
 
-        '''
-        print("Max probability")
-        print(max_probability)
-        print("Max index")
-        print(max_aa_index)
-        '''
         # save to dictionary whole sequence of most probabible aminoacids on each indexes on each node
         ancestrals_dict[node_key].insert(index, header_aa_list[max_aa_index])
 
@@ -130,13 +142,11 @@ def writeSequencesToOutputFolder(tree, ancestrals_dict, output_folder):
         tree_node_id = clade.confidence
 
         if (tree_node_id != None):
-            #print(ancestrals_dict[tree_node_id])
             file_name = "node_" + str(tree_node_id) + ".fas"
             f = open(output_folder + file_name,"w+")
             sequence = listToString(ancestrals_dict[tree_node_id])
             f.write(sequence + "\n")
             f.close()
-
 
 # for every sequence (e.g XP_007318498.1) I got all hierarchic parents and their confidence codes
 # dictionary is in form Node : [names of sequences (e.g XP_007318498.1) which are descendants of this node]
@@ -152,8 +162,8 @@ def getSequencesOnNode(tree, sequences_id, parents):
                 branch_lengths_to_nodes = []
                 branch_lengths_to_nodes.append(float(clade.branch_length))
                 all_parents, branch_lengths_to_nodes = findAllParentsRecursively(parents, clade, [], branch_lengths_to_nodes)
-                #print(all_parents)
 
+                # add all keys into dictionary with init value of []
                 for p in all_parents:
                     sequences_on_node[p] = []
                     branch_lengths_on_node[p] = []
@@ -161,18 +171,13 @@ def getSequencesOnNode(tree, sequences_id, parents):
 
     # insert all sequences which are connected in hierarchy to this node
     for sequence_id in sequences_id:
-        #print(sequence_id)
 
         for clade in tree.find_clades():
-            #print(clade.name)
-            #print(sequence_id)
             if (str (clade.name) == str (sequence_id)):
-                #print("FOUND")
                 all_parents = []
                 branch_lengths_to_nodes = []
                 branch_lengths_to_nodes.append(float(clade.branch_length))
                 all_parents, branch_lengths_to_nodes = findAllParentsRecursively(parents, clade, [], branch_lengths_to_nodes)
-                #print(branch_lengths_to_nodes)
 
                 i = 0
                 branch_lengths_to_sum = []
@@ -183,12 +188,8 @@ def getSequencesOnNode(tree, sequences_id, parents):
                         branch_lengths_to_sum.append(branch_length)
                     else:
                         branch_lengths_to_sum = branch_lengths_to_nodes[0:i+1]
-                    #print(branch_lengths_to_sum)
                     total_branch_lengths_to_nodes.append(sum(branch_lengths_to_sum))
                     i += 1
-
-                #print(total_branch_lengths_to_nodes)
-                #print(all_parents)
 
                 j = 0
                 for p in all_parents:
@@ -210,17 +211,16 @@ def getPositionsToInsertGape(sequences_on_node, sequence_length, records):
     for key, value in sequences_on_node.items():
         # init arrays to store count of gapes for each index and count of aminoacids for each index across sequnces connected to this node
         gapes_count = [0] * sequence_length
-        #print(gapes_count)
         aa_count = [0] * sequence_length
         for v in value:
-            #print(v)
             for r in records:
                 if (v == r.name):
                     sequence_str = str(r.seq)
                     sequence_list = split(sequence_str)
-                    #print(sequence_list)
+
                     i = 0
                     for s in sequence_list:
+                        # count occurences of gape or aminoacid on specific index
                         if (s == '-'):
                             old_count = gapes_count[i]
                             new_count = old_count + 1
@@ -230,15 +230,14 @@ def getPositionsToInsertGape(sequences_on_node, sequence_length, records):
                             new_count = old_count + 1
                             aa_count[i] = new_count
                         i += 1
-                    #print(gapes_count)
-                    #print(aa_count)
 
         j = 0
         for g, a in zip(gapes_count, aa_count):
+            # if gapes count is higher, append this index as one where gape should be inserted
             if (g > a):
                 positions_to_insert_gape[key].append(j)
             j += 1
-    #print(positions_to_insert_gape)
+
     return positions_to_insert_gape
 
 # calculate positions to insert gape for each ancestral node
@@ -249,28 +248,25 @@ def getPositionsToInsertGapeBranchLengthFeature(sequences_on_node, branch_length
         positions_to_insert_gape[key] = []
 
     for key, value in sequences_on_node.items():
-        print(key)
         # init arrays to store count of gapes for each index and count of aminoacids for each index across sequnces connected to this node
         branch_lengths_list = branch_lengths_on_node[key]
-        #print(branch_lengths_list)
         gapes_count = [0] * sequence_length
-        #print(gapes_count)
         aa_count = [0] * sequence_length
 
         k = 0
         for v in value:
-            print(v)
             # get particular length of branch to specific sequence
             for r in records:
                 if (v == r.name):
                     branch_length = branch_lengths_list[k]
-                    print(branch_length)
+
                     k += 1
                     sequence_str = str(r.seq)
                     sequence_list = split(sequence_str)
-                    #print(sequence_list)
+
                     i = 0
                     for s in sequence_list:
+                        # count of occurence of gape or aminoacid but instead of adding 1 we add branch distance
                         if (s == '-'):
                             old_count = gapes_count[i]
                             new_count = old_count + branch_length
@@ -280,15 +276,14 @@ def getPositionsToInsertGapeBranchLengthFeature(sequences_on_node, branch_length
                             new_count = old_count + branch_length
                             aa_count[i] = new_count
                         i += 1
-                    #print(gapes_count)
-                    #print(aa_count)
 
         j = 0
         for g, a in zip(gapes_count, aa_count):
+            # if gapes count is higher, append this index as one where gape should be inserted
             if (g > a):
                 positions_to_insert_gape[key].append(j)
             j += 1
-    #print(positions_to_insert_gape)
+
     return positions_to_insert_gape
 
 # insert gapes to positions in output sequence
@@ -296,31 +291,25 @@ def insertGapesToSequence(ancestrals_dict, positions_to_insert_gape):
     for ancestral_key, ancestral_value in ancestrals_dict.items():
         probable_sequence = ancestrals_dict[ancestral_key]
         gape_node_positions = positions_to_insert_gape[ancestral_key]
-        #print(gape_node_positions)
+
         k = 0
         aa_new_codes = []
         for aa_code in probable_sequence:
-            #print(positions_to_insert_gape)
-            #print(k)
-            #print(aa_code)
+            # insert gape on positions previously computed
             if (k in gape_node_positions):
                 aa_new_codes.append("-")
             else:
                 aa_new_codes.append(aa_code)
             k += 1
 
-        #print(aa_new_codes)
-
         ancestrals_dict[ancestral_key] = aa_new_codes
     return ancestrals_dict
 
 
 
-
-
 # read phylogenetic tree
 tree = Phylo.read("./files/tree.tre", "newick")
-print(tree)
+# print input tree in ASCII form
 Phylo.draw_ascii(tree)
 
 ancestrals_dict = getMostProbableAminoacids()
@@ -335,40 +324,38 @@ for r in records:
 ################################## CALCULATE WITH ONLY MOST PROBABLE AMINOACIDS WITH POSTERIOR PROBABILITES (WITHOUT INSERTING GAPS) ######################################################################
 found_leaves = []
 writeSequencesToOutputFolder(tree, ancestrals_dict, "./results/probabilities/")
+print("ANCESTRAL SEQUENCES WITH USING ONLY MOST PROBABLE AMINOACIDS WRITTEN INTO ./results/probabilities/")
 
 #################################### CALCULATE WITH INSERTING GAPS BASED ON GAPS OCCURENCE FREQUENCE ######################################################################
 # get all parents of tree
 parents = all_parents(tree)
-#print(parents)
 
+# get sequence which are connected to each node and branch length from each node (branch length is used as feature in CALCULATE WITH INSERTING GAPS BASED ON BRANCH LENGTH FEATURE)
 sequences_on_node,  branch_lengths_on_node = getSequencesOnNode(tree, sequences_id, parents)
-#print(sequences_on_node)
 
+# get positions where to insert gape to each ancestral sequence
 positions_to_insert_gape = getPositionsToInsertGape(sequences_on_node, sequence_length, records)
-print(positions_to_insert_gape)
 
-
+# insert gapes to each sequence and get dict in form Node Id : list of chars of sequence e.g. 66 : ['-','G','V',...]
 ancestrals_dict = insertGapesToSequence(ancestrals_dict, positions_to_insert_gape)
 
+# write final sequences to separate files based on ancestral node id into specific results folder
 writeSequencesToOutputFolder(tree, ancestrals_dict, "./results/gapes/")
+print("ANCESTRAL SEQUENCES WITH USING GAPS FREQUENCE (WITHOUT BRANCH LENGTH FEATURE) WRITTEN INTO ./results/gapes/")
 
 #################################### CALCULATE WITH INSERTING GAPS BASED ON BRANCH LENGTH FEATURE ######################################################################
+# get all parents of tree
 parents = all_parents(tree)
-print(parents)
 
+#  get sequence which are connected to each node and branch length from each node (branch lengths are added when necessary, see Assignment of project)
 sequences_on_node,  branch_lengths_on_node = getSequencesOnNode(tree, sequences_id, parents)
-print(sequences_on_node)
-print(branch_lengths_on_node)
 
-
-#for node_key, connected_node_sequences in sequences_on_node.items():
-#    connected_node_sequences_list = sequences_on_node[node_key]
-
-
-#positions_to_insert_gape = getPositionsToInsertGape(sequences_on_node, sequence_length, records)
+# get positions where to insert gape to each ancestral sequence
 positions_to_insert_gape = getPositionsToInsertGapeBranchLengthFeature(sequences_on_node, branch_lengths_on_node, sequence_length, records)
-print(positions_to_insert_gape)
 
+# insert gapes to each sequnce and get dict in form Node Id : list of chars of sequence e.g. 66 : ['-','G','V',...]
 ancestrals_dict = insertGapesToSequence(ancestrals_dict, positions_to_insert_gape)
 
+# write final sequences to separate files based on ancestral node id into specific results folder
 writeSequencesToOutputFolder(tree, ancestrals_dict, "./results/gapes_branch_distances/")
+print("ANCESTRAL SEQUENCES WITH USING GAPS FREQUENCE AND BRANCH LENGTH FEATURE WRITTEN INTO ./results/gapes_branch_distances/")
